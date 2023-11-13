1906b1ae92f342de28839e5acf92ff5cf187743b
Connor Richmond <richmond.connor@gmail.com>
2023-11-13 12:15:46 -0500

    setup base url

---
+++ app/services/place_service.rb
@@ -0,0 +1,10 @@
+class PlaceService
+
+  def conn
+    Faraday.new(url: "https://api.geoapify.com/v2/places") do |faraday|
+      faraday.params["catagories"] = "tourism"
+      faraday.params["lang"] = "en"
+      faraday.params["apiKey"] = Rails.application.credentials.places.[:key]
+    end
+  end
+end
