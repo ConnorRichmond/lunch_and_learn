b0fa92306084a46acd4691d22c836168d2c245c9
Connor Richmond <richmond.connor@gmail.com>
2023-11-15 04:48:10 -0500

    seeds for postman

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,25 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+user1 = User.create(
+  name: "user1",
+  email: "user1@gmail.com",
+  password: "1234",
+  api_key: "1234"
+)
+
+user2 = User.create(
+  name: "user2",
+  email: "user2@gmail.com",
+  password: "1234",
+  api_key: "1234"
+)
+
+user.favorites.create(
+  country: "country 1",
+  recipe_link: "https://recipe_link_1.com",
+  recipe_title: "recipe 1"
+)
+
+user.favorites.create(
+  country: "country 2",
+  recipe_link: "https://recipe_link_2.com",
+  recipe_title: "recipe 2"
+)
