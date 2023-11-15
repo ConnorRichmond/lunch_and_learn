22c17f4704334a19012e79858252c9aedf1a9f34
Connor Richmond <richmond.connor@gmail.com>
2023-11-15 03:48:24 -0500

    update create controllers for handling user favorites

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
   namespace :api do
     namespace :v1 do
       resources :recipes, only: [:index]
+      resources :learning_resources, only: [:index]
+      resources :users, only: [:create]
+      resources :sessions, only: [:create]
+      resources :favorites, only: [:create, :index]
     end
   end
 end
